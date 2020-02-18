aa2bc41fccdf8193b999dc8d90663c63564a3d19
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-18 12:10:25 +0200

    init

--- InvertSelfieImage/ContentView.swift
+++ InvertSelfieImage/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Image(systemName: "person")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .rotation3DEffect(.degrees(45), axis: (x: -1, y: 0, z: 0))
     }
 }
 
